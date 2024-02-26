a1bf91f7f52471f0cdcca2d3f86df09e06b3e78b
@bips241 <biplabmal626@gmail.com>
2024-02-27 03:58:17 +0530

    css_clash_remove

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Container, Row, Col } from 'react-bootstrap';
 import images from "../../constants/images";
-import './Home.css';
+
 
 
 const Home = () => {
@@ -21,8 +21,99 @@ const Home = () => {
 
     return (
         <><style>
-            @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,800&display=swap')
-        </style>
+        {`
+            @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,800&display=swap');
+
+            .carousel {
+                overflow: hidden;
+            }
+
+            .carousel-caption h1 {
+                height: 100px;
+                font-family: "Poppins", sans-serif;
+                font-weight: 800;
+                font-style: italic;
+                color: teal;
+                font-size: 70px;
+                margin: 0;
+            }
+
+            .carousel-caption h2 {
+                font-family: "Poppins", sans-serif;
+                font-weight: 800;
+                font-style: italic;
+                height: 90px;
+                font-size: 60px;
+                margin: 0;
+            }
+
+            .carousel-caption h3 {
+                font-family: "Poppins", sans-serif;
+                font-weight: 800;
+                font-style: italic;
+                height: 90px;
+                font-size: 45px;
+                margin: 0;
+            }
+
+            .carousel-item img {
+                width: 100vw;
+                height: 100vh;
+                object-fit: cover;
+            }
+
+            .carousel-caption {
+                position: absolute;
+                top: 0;
+                left: 0;
+                right: 0;
+                bottom: 0;
+                text-align: center;
+                overflow: hidden;
+            }
+
+            .carousel-container {
+                overflow: hidden;
+            }
+
+            .btn {
+                background-color: #008080;
+                color: white;
+                border-color: white;
+                border-width: 2px;
+                transform: translateY(150px);
+                font-weight: bold;
+                box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+                width: 150px !important;
+            }
+
+            .btn:hover {
+                background-color: #009999;
+                color: white;
+            }
+
+            @media (max-width: 768px) {
+                .carousel-caption h1 {
+                    font-size: 40px;
+                    height: 90px;
+                    margin: 5px;
+                }
+
+                .carousel-caption h2 {
+                    font-size: 30px;
+                    height: 45px;
+                    margin-bottom: 0;
+                }
+
+                .carousel-caption h3 {
+                    font-size: 25px;
+                    height: 70px;
+                    margin: 5px;
+                    margin-top: 0;
+                }
+            }
+        `}
+    </style>
         <Container fluid>
                 <Row>
                     <Col style={{ padding: 0 }}>
