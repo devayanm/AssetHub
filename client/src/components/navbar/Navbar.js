0de8d8c1f748d262673485155c520483d7496f9d
Devayan <devayan9689@gmail.com>
2024-03-03 20:41:08 +0530

    Removed .env and updated navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,79 +1,230 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { RxHamburgerMenu } from "react-icons/rx";
-import { ImCross } from "react-icons/im";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faHome,
+    faBook,
+    faTachometerAlt,
+    faUserPlus,
+    faSignInAlt,
+    faUser,
+    faSignOutAlt,
+    faTimes
+} from '@fortawesome/free-solid-svg-icons';
+import { useUserAuth } from '../../context/UserAuthContext';
 import images from "../../constants/images.js";
-import './Navbar.css';
 
-const Navbar = ({ links, isLoggedIn }) => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const [toggleMenu, setToggleMenu] = useState(false);
+const Navbar = () => {
+    const { user, logOut } = useUserAuth();
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
+    const handleToggleNav = () => {
+        setIsNavCollapsed(!isNavCollapsed);
+    };
 
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    const toggleMenuHandler = () => {
-        setToggleMenu(prevState => !prevState);
+    const handleLogout = () => {
+        logOut();
+        setIsNavCollapsed(true);
     };
 
     return (
-        <nav className="app__navbar container-fluid">
-            <div className="app__navbar-logo">
-                <div className="overlay"></div>
-                <img src={images.logo} alt="logo" />
-            </div>
-            {windowWidth < 601 ? (
-                <><div className="log_in">
-                    {!isLoggedIn && (
-                        <><button type="button" class="btn">log in</button>
-                        </>
-                    )}
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark d-lg-none">
+                <div className="container" style={{ display: 'flex', alignItems: 'center' }}>
+                    <Link to="/" className="navbar-brand">
+                        <img
+                            src={images.logo}
+                            alt="Asset-Hub"
+                            height="100"
+                            className="d-inline-block align-top m-1"
+                        />
+                    </Link>
+                    <button
+                        style={{ marginLeft: 'auto', backgroundColor: 'transparent', border: 'none', cursor: 'pointer' }}
+                        type="button"
+                        onClick={handleToggleNav}
+                    >
+                        <span style={{ color: 'black', fontSize: '1.5rem' }}>&#9776;</span>
+                    </button>
                 </div>
-                    <div className="hamburger-menu">
-                        {toggleMenu ? (
-                            <ImCross className="overlay__close" onClick={toggleMenuHandler} />
+
+                <div
+                    style={{
+                        position: 'fixed',
+                        top: 0,
+                        left: isNavCollapsed ? '-300px' : '0',
+                        width: '300px',
+                        height: '100%',
+                        backgroundColor: '#333',
+                        colopr:'white',
+                        zIndex: '1000',
+                        transition: 'left 0.3s ease-in-out',
+                        overflowY: 'auto',
+                    }}
+                >
+                    <button
+                        style={{ position: 'absolute', top: '10px', right: '10px', backgroundColor: 'transparent', border: 'none', color: 'white', fontSize: '1.5rem', cursor: 'pointer' }}
+                        onClick={handleToggleNav}
+                    >
+                        <FontAwesomeIcon icon={faTimes} />
+                    </button>
+                    <ul style={{ listStyle: 'none', padding: '20px' }}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                <FontAwesomeIcon icon={faHome} className="mr-1" />
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                <FontAwesomeIcon icon={faBook} className="mr-1" />
+                                About Us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                <FontAwesomeIcon icon={faTachometerAlt} className="mr-1" />
+                                Explore
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                <FontAwesomeIcon icon={faUser} className="mr-1" />
+                                Dashboard
+                            </Link>
+                        </li>
+                        {user ? (
+                            <>
+                                <li className="nav-item">
+                                    <Link to="/profile" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                        <FontAwesomeIcon icon={faUser} className="mr-1" />
+                                        Profile
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <button className="nav-link btn btn-link" onClick={handleLogout} style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                        <FontAwesomeIcon icon={faSignOutAlt} className="mr-1" />
+                                        Logout
+                                    </button>
+                                </li>
+                            </>
                         ) : (
-                            <RxHamburgerMenu className="Burger" onClick={toggleMenuHandler} />
+                            <>
+                                <li className="nav-item">
+                                    <Link to="/login" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                        <FontAwesomeIcon icon={faSignInAlt} className="mr-1" />
+                                        Login
+                                    </Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link to="/register" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                        <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
+                                        Register
+                                    </Link>
+                                </li>
+                            </>
                         )}
-                        {toggleMenu && (
-                            <div className="burger__list">
-                                <ul className="burger__list-links">
-                                    {links.map((link, index) => (
-                                        <li key={index} className="bl" id={index}>
-                                            <a href={link.url}>{link.text}</a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        )}
-                    </div></>
-            ) : (
-                <div>
-                    <div className="log_in">
-                        {!isLoggedIn && (
-                            <Link to="/auth/signin" className="btn2">Log In</Link>
-                        )}
-                    </div>
-                    <ul className="app__navbar-links">
-                        {links.map((link, index) => (
-                            <li className="p__opensans" key={index}>
-                                <a href={link.url}>{link.text}</a>
-                            </li>
-                        ))}
                     </ul>
                 </div>
-            )}
-        </nav>
+                <div
+                    style={{
+                        display: isNavCollapsed ? 'none' : 'block',
+                        position: 'fixed',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        background: 'rgba(0, 0, 0, 0.5)',
+                        color: 'black',
+                        zIndex: '999',
+                    }}
+                    onClick={handleToggleNav}
+                ></div>
+            </nav>
+
+            <nav className="navbar navbar-expand-lg navbar-light d-none d-lg-block">
+                <div className="container">
+                    <Link to="/" className="navbar-brand">
+                        <img
+                            src={images.logo}
+                            alt="Asset-Hub"
+                            height="100"
+                            className="d-inline-block align-top m-1"
+                        />
+                    </Link>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse justify-content-lg-end" id="navbarNav">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                    <FontAwesomeIcon icon={faHome} className="mr-1" />
+                                    Home
+                                </Link>
+                            </li>
+                            {user ? (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faBook} className="mr-1" />
+                                            About Us
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faTachometerAlt} className="mr-1" />
+                                            Explore
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faUser} className="mr-1" />
+                                            Dashboard
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/profile" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faUser} className="mr-1" />
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <button className="nav-link btn btn-link" onClick={handleLogout} style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faSignOutAlt} className="mr-1" />
+                                            Logout
+                                        </button>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <li className="nav-item">
+                                        <Link to="/login" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faSignInAlt} className="mr-1" />
+                                            Login
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/register" className="nav-link" style={{ fontSize: '1.2rem', marginRight: '15px' }}>
+                                            <FontAwesomeIcon icon={faUserPlus} className="mr-1" />
+                                            Register
+                                        </Link>
+                                    </li>
+                                </>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserAuthProvider } from './context/UserAuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </Router>
+    <UserAuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </Router>
+    </UserAuthProvider>
   </React.StrictMode>
 );
