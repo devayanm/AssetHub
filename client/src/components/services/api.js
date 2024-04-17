ae7e11512e3cf922a0cc6274ac6e7a104dafd774
Devayan <devayan9689@gmail.com>
2024-04-17 11:45:08 +0530

    changes

--- client/src/components/services/api.js
+++ client/src/components/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000/api/v1';
+
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: baseURL,
 });
 
 export const userApi = {
